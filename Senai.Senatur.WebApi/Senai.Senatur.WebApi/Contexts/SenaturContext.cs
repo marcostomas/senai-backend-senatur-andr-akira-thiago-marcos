aa6be4f223379344ef22cde569424a1c349397db
André Akira Kusaba <43036207856@info.local>
2020-03-10 11:26:09 -0300

    Senatur 10/03 (1)

--- Senai.Senatur.WebApi/Senai.Senatur.WebApi/Contexts/SenaturContext.cs
+++ Senai.Senatur.WebApi/Senai.Senatur.WebApi/Contexts/SenaturContext.cs
@@ -24,8 +24,9 @@ namespace Senai.Senatur.WebApi.Domains
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                /*optionsBuilder.UseSqlServer("Data Source=DEV1201\\SQLEXPRESS; Initial Catalog=Senatur_Manha; user Id=sa; pwd=sa@132;");*/
-                optionsBuilder.UseSqlServer("Data Source=DEV801\\SQLEXPRESS; Initial Catalog=Senatur_Manha; user Id=sa; pwd=sa@132;");
+                optionsBuilder.UseSqlServer("Data Source=N-1S-DEV-17\\SQLEXPRESS; Initial Catalog=Senatur_Manha; user Id=sa; pwd=sa@132;");
+                //optionsBuilder.UseSqlServer("Data Source=DEV801\\SQLEXPRESS; Initial Catalog=Senatur_Manha; user Id=sa; pwd=sa@132;");
+                //optionsBuilder.UseSqlServer("Data Source=DEV1201\\SQLEXPRESS; Initial Catalog=Senatur_Manha; user Id=sa; pwd=sa@132;");
             }
         }
 
--- Senai.Senatur.WebApi/Senai.Senatur.WebApi/Startup.cs
+++ Senai.Senatur.WebApi/Senai.Senatur.WebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace Senai.Senatur.WebApi
 {
@@ -15,6 +16,14 @@ namespace Senai.Senatur.WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                })
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +34,7 @@ namespace Senai.Senatur.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
