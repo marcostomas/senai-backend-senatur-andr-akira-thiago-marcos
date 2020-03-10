902748ad8b2eb3bc7f3b04c6b66c467e408e22e2
Thihenriquesilva <henriquesilva322@outlook.com>
2020-03-10 10:13:34 -0300

    DataFirst

---
+++ Senai.Senatur.WebApi/Senai.Senatur.WebApi/Contexts/SenaturContext.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Senai.Senatur.WebApi.Domains
+{
+    public partial class SenaturContext : DbContext
+    {
+        public SenaturContext()
+        {
+        }
+
+        public SenaturContext(DbContextOptions<SenaturContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TabelaPacotes> TabelaPacotes { get; set; }
+        public virtual DbSet<TipoUsuario> TipoUsuario { get; set; }
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DEV1201\\SQLEXPRESS; Initial Catalog=Senatur_Manha; user Id=sa; pwd=sa@132;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TabelaPacotes>(entity =>
+            {
+                entity.HasKey(e => e.IdPacote);
+
+                entity.ToTable("Tabela_Pacotes");
+
+                entity.Property(e => e.IdPacote).HasColumnName("ID_Pacote");
+
+                entity.Property(e => e.DataIda).HasColumnType("date");
+
+                entity.Property(e => e.DataVolta).HasColumnType("date");
+
+                entity.Property(e => e.Descricao).HasColumnType("text");
+
+                entity.Property(e => e.NomeCidade)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomePacote)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TipoUsuario>(entity =>
+            {
+                entity.HasKey(e => e.IdTipoUsuario);
+
+                entity.HasIndex(e => e.Titulo)
+                    .HasName("UQ__TipoUsua__7B406B5689D4A93D")
+                    .IsUnique();
+
+                entity.Property(e => e.IdTipoUsuario).HasColumnName("ID_TipoUsuario");
+
+                entity.Property(e => e.Titulo)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.HasIndex(e => e.Email)
+                    .HasName("UQ__Usuarios__A9D105347C0D422D")
+                    .IsUnique();
+
+                entity.Property(e => e.IdUsuario).HasColumnName("ID_Usuario");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IdTipoUsuario).HasColumnName("ID_TipoUsuario");
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdTipoUsuarioNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdTipoUsuario)
+                    .HasConstraintName("FK__Usuarios__ID_Tip__4D94879B");
+            });
+        }
+    }
+}
---
+++ Senai.Senatur.WebApi/Senai.Senatur.WebApi/Domains/TipoUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Senai.Senatur.WebApi.Domains
+{
+    public partial class TipoUsuario
+    {
+        public TipoUsuario()
+        {
+            Usuarios = new HashSet<Usuarios>();
+        }
+
+        public int IdTipoUsuario { get; set; }
+        public string Titulo { get; set; }
+
+        public ICollection<Usuarios> Usuarios { get; set; }
+    }
+}
