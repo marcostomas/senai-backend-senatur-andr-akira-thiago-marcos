de8b4046d43a01027eb3f77aaa856e10055790a4
André Akira Kusaba <43036207856@info.local>
2020-03-11 10:14:33 -0300

    Não me julguem

---
+++ Senai.Senatur.WebApi/Senai.Senatur.WebApi/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Senai.Senatur.WebApi.Domains;
+using Senai.Senatur.WebApi.Interfaces;
+using Senai.Senatur.WebApi.Repositories;
+using Senai.Senatur.WebApi.ViewModel;
+
+namespace Senai.Senatur.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository { get; set; }
+
+        public LoginController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Post(LoginViewModel login)
+        {
+            Usuarios usuarioBuscado = _usuarioRepository.BuscarPorEmailSenha(login.Email, login.Senha);
+
+            if(usuarioBuscado == null)
+            {
+                return NotFound("Email ou senha inválida");
+            }
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.IdUsuario.ToString()),
+                new Claim(ClaimTypes.Role, usuarioBuscado.IdTipoUsuario.ToString()),
+                new Claim("Claim personalizada", "Valor teste")
+            };
+            // Define a chave de acesso ao token
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("senatur-chave-autenticacao"));
+            // Define as credenciais do token - Header
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            // Gera o token
+            var token = new JwtSecurityToken(
+                issuer: "Senatur.WebApi",                // emissor do token
+                audience: "Senatur.WebApi",              // destinatário do token
+                claims: claims,                         // dados definidos acima
+                expires: DateTime.Now.AddMinutes(30),   // tempo de expiração
+                signingCredentials: creds               // credenciais do token
+            );
+            // Retorna Ok com o token
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
+    }
+
+}
